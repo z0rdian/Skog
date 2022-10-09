8b58162861afde53297070ea1391cf25345e3750
z0rdian <valentinjva@gmail.com>
2022-10-08 23:07:58 -0300

    font-family try 2

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,22 +1,22 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-    render() {
-        return (
-            <Html lang='en'>
-                <Head>
-                    <link
-                        href='https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&family=Yantramanav:wght@300;400;500;700;900&display=swap'
-                        rel='stylesheet'
-                    />
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
