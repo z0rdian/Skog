b48b98efead1ac1c88bf2d3992adfa686dcf21f4
z0rdian <valentinjva@gmail.com>
2022-10-21 01:38:30 -0300

    mp4 mobile debug try 1

--- src/components/Home/HeroContainerArea.jsx
+++ src/components/Home/HeroContainerArea.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Header from '../common/Header';
 import HeroArea from './HeroArea';
-import videoBg from '../assets/videoBg.mp4'
 
 const HeroContainerArea = () => {
   return (
     <div className='videoContainer'>
-      <video src={videoBg } autoPlay loop muted />
+      <video src={require('../assets/videoBg.mp4')} autoPlay loop muted />
         <Header className="nav-header-container" />
         <HeroArea className="hero-container" />
     </div>
