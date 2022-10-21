fa2100e0881aa44bb1b615a4a5cf5a47c0b1fefb
z0rdian <valentinjva@gmail.com>
2022-10-21 11:50:28 -0300

    try4

--- next.config.js
+++ next.config.js
@@ -7,10 +7,4 @@ module.exports = nextConfig
 
 const withVideos = require('next-videos')
 
-module.exports = withVideos({
-  basePath: '..public/src/components/assets/videoBg.mp4',
-
-  webpack(config, options) {
-    return config
-  }
-})
+module.exports = withVideos()
